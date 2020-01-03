fb68c18788dc67827cc9b225833bc9c090f52cae
Anantha Krishnan R <54615043+ananthk324@users.noreply.github.com>
2020-01-03 22:12:57 +0530

    Count and Sort

---
+++ countAndSort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int countAndSort(int in_array[], int max_num, int size)
+{
+    int temp_array[max_num + 1], out_array[size], i, j, k;
+    
+    for(i = 0; i <= max_num; i++)
+        temp_array[i] = 0;
+    
+    for(i = 0; i <= size; i++)
+        temp_array[in_array[i]] += 1;
+    
+    k=0;
+    for(j = 0; j <= max_num; j++)
+    {
+        for(i = 0; i < temp_array[j]; i++)
+        {
+            out_array[k] = j;
+            k++;
+        }
+    }
+    
+    printf("\nSorted Array: ");
+    for(i = 0; i <= size; i++)
+        printf("%d ", out_array[i]);
+	
+	return 0;
+}
+
+int main()
+{
+    int* in_array;
+    int size, max_num, i;
+
+    printf("\nCount and Sort\nEnter array size, max num: ");
+    scanf("%d %d", &size, &max_num);
+    
+    in_array = (int*)malloc((sizeof(int)) * size);
+    
+    printf("\nInput array elements: ");
+    for(i = 0; i <= size; i++)
+        scanf("%d", &in_array[i]);
+    
+    countAndSort(in_array, max_num, size);
+    return 0;
+}
